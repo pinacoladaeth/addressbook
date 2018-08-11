1c2b78b1183cc483e84d9262be0d3bc61151cb35
Eric Tu <tu.eric@hotmail.com>
2018-08-11 14:45:04 +0100

    address only

--- contract/Pinacolada.sol
+++ contract/Pinacolada.sol
@@ -1,20 +1,34 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.18;
 
 import "./ENS.sol"; 
 import "./TemporaryHashRegistrar.sol"; 
 import "./PublicResolver.sol"; 
 import "./ReverseRegistrar.sol"; 
+import "./strings.sol";
 
 contract Pinacolada {
   ENS public registry;
   Registrar public registrar;
   PublicResolver public resolver;
   ReverseRegistrar public reverseRegistrar;
+  mapping (address => address[]) public graph;
+  event Connection (address from, address to);
 
-  constructor (address registryA, address registrarA, address resolverA, address reverseRegistrarA) public{
-    registry = ENS(registryA);
-    registrar = Registrar(registrarA);
-    resolver = PublicResolver(resolverA);
-    reverseRegistrar = ReverseRegistrar(reverseRegistrarA);
-  }
+
+//   function Pinacolada (address registryA, address registrarA, address resolverA, address reverseRegistrarA) public{
+//     registry = ENS(registryA);
+//     registrar = Registrar(registrarA);
+//     resolver = PublicResolver(resolverA);
+//     reverseRegistrar = ReverseRegistrar(reverseRegistrarA);
+//   }
+  
+
+    function registerFriend(address _friend) public {
+        graph[msg.sender].push(_friend);
+        Connection(msg.sender, _friend);
+    }
+    
+    function isMember(address _person) public view returns (bool) {
+        return graph[_person].length != 0;
+    }
 }
